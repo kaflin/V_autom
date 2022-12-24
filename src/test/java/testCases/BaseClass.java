c43d00f5e5869702ba9597f621a95e165079a99a
suraj_gupta <suraj520876@gmail.com>
2022-12-24 19:23:42 +0545

    Added driver

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -1,8 +1,11 @@
 package testCases;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
@@ -17,14 +20,20 @@ public class BaseClass {
     public String password = readConfig.getPassword();
     public static WebDriver webDriver;
     public static Logger logger;
+    Actions action;
 
     @Parameters("browser")
     @BeforeClass
     public void setup(@Optional("chrome") String br) {
+        logger = Logger.getLogger("Dallcon");
+        PropertyConfigurator.configure("log4j.properties");
         if (br.equals("chrome")) {
-            String driverPath="/home/suraj/Downloads/selenium/chromedriver_linux64/chromedriver";
-            System.setProperty("webdriver.chrome.driver", driverPath);
-            webDriver = new ChromeDriver();
+//            String driverPath="/home/suraj/Downloads/selenium/chromedriver_linux64/chromedriver";
+//            System.setProperty("webdriver.chrome.driver", driverPath);
+//            webDriver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            System.setProperty("webdriver.chrome.driver", readConfig.getChromePath());
+            webDriver =new ChromeDriver(options);
             webDriver.manage().window().maximize();
         }
         webDriver.get(baseURL);
--- src/test/java/testCases/TC_LoginTest_001.java
+++ src/test/java/testCases/TC_LoginTest_001.java
@@ -6,101 +6,97 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.LoginPage;
-
-import java.io.IOException;
 import java.util.List;
 
 public class TC_LoginTest_001 extends BaseClass {
 
     @Test(priority = 1)
     public void loginTest() throws InterruptedException {
-        System.out.println("URL is opened");
+        logger.info("URL is opened");
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.setUser(user);
-        System.out.println("Entered UserName");
+        logger.info("Entered Username");
 
         loginPage.setPassword(password);
-        System.out.println("Entered password");
+        logger.info("Entered password");
         loginPage.clickSubmit();
 
         if (webDriver.getTitle().equals("Swag Labs")) {
             Assert.assertTrue(true);
-            System.out.println("Logging Test passed");
+            logger.info("Logging Test passed");
         } else {
             Assert.assertTrue(false);
-            System.out.println("Logging Test failed");
+            logger.info("Logging Test failed");
         }
         Thread.sleep(2000);
     }
 
     @Test(priority = 2)
     public void addToCart() throws InterruptedException {
-        System.out.println("Adding Item to cart");
-        webDriver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        logger.info("Adding Sauce Labs Backpack to cart");
+        WebElement element =webDriver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']"));
+        action=new Actions(webDriver);
+        action.moveToElement(element).click().perform();
         Thread.sleep(2000);
-
     }
 
     @Test(priority = 3)
     public void assertQuantityAndItemDescription() throws InterruptedException {
-        System.out.println("Asserting the quantity and Item Description");
+        logger.info("Asserting the quantity and Item Description");
         String quantity = webDriver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText();
         if (quantity.equals("1")) {
-            System.out.println("Only one item added to cart");
+            logger.info("Only one item added to cart");
             Assert.assertTrue(true);
         } else {
-            System.out.println("More the one item added to cart");
+            logger.info("More the one item added to cart");
             Assert.assertTrue(false);
         }
-        System.out.println("Clicking on Cart for item Description");
+        logger.info("Clicking on Cart for item Description");
         //Instantiating Actions class
-        Actions actions = new Actions(webDriver);
+        action = new Actions(webDriver);
         WebElement cart = webDriver.findElement(By.xpath("//a[@class='shopping_cart_link']"));
         //To mouseover on cart
-        actions.moveToElement(cart);
-
-        //build()- used to compile all the actions into a single step
-        actions.click().build().perform();
+        action.moveToElement(cart).click().perform();
         Thread.sleep(2000);
 
         String itemDescription = webDriver.findElement(By.xpath("//div[@class='inventory_item_desc']")).getText();
         if (itemDescription.equals("carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.")) {
-            System.out.println("Item description of Sauce Labs Backpack");
+            logger.info("Item description of Sauce Labs Backpack");
             Assert.assertTrue(true);
         } else {
-            System.out.println("Not an Item description of Sauce Labs Backpack");
+            logger.info("Not an Item description of Sauce Labs Backpack");
             Assert.assertTrue(false);
         }
         Thread.sleep(2000);
-
-
     }
 
     @Test(priority = 4)
-    public  void removeItemAndAssertToEmpty() throws InterruptedException {
-        System.out.println("Removing the Element from cart");
+    public void removeItemAndAssertToEmpty() throws InterruptedException {
+        logger.info("Removing the Element from cart");
         webDriver.findElement(By.id("remove-sauce-labs-backpack")).click();
-        System.out.println("Item Removed");
+        logger.info("Item Removed");
         Thread.sleep(2000);
-        List<WebElement> quant=webDriver.findElements(By.xpath("//span[@class='shopping_cart_badge']"));
-        if(quant.size()==0){
-            System.out.println("Cart is empty");
+        List<WebElement> quant = webDriver.findElements(By.xpath("//span[@class='shopping_cart_badge']"));
+        if (quant.size() == 0) {
+            logger.info("Cart is empty");
             Assert.assertTrue(true);
-        }else{
-            System.out.println("Cart is not empty");
+        } else {
+            logger.info("Cart is not empty");
             Assert.assertTrue(false);
         }
         Thread.sleep(2000);
     }
+
     @Test(priority = 5)
     public void addAnotherItemToCart() throws InterruptedException {
         webDriver.findElement(By.id("continue-shopping")).click();
-        System.out.println("Adding Another Item to cart");
+        logger.info("Adding Another Item to cart");
         webDriver.findElement(By.id("add-to-cart-test.allthethings()-t-shirt-(red)")).click();
         Thread.sleep(2000);
-        System.out.println("Item Added");
+        logger.info("Item Added");
 
     }
+
     @Test(priority = 6)
     public void checkOut() throws InterruptedException {
         Actions actions = new Actions(webDriver);
@@ -112,21 +108,22 @@ public class TC_LoginTest_001 extends BaseClass {
         actions.click().build().perform();
         Thread.sleep(2000);
         webDriver.findElement(By.id("checkout")).click();
-        System.out.println("Checked out");
+        logger.info("Checked out");
 
     }
     @Test(priority = 7)
-    public void clickOnContinue(){
+    public void clickOnContinue() {
         webDriver.findElement(By.id("continue")).click();
-        String message=webDriver.findElement(By.xpath("//div[@class='error-message-container error']")).getText();
-        if(message.equals("Error: First Name is required")){
+        String message = webDriver.findElement(By.xpath("//div[@class='error-message-container error']")).getText();
+        if (message.equals("Error: First Name is required")) {
             Assert.assertTrue(true);
-            System.out.println("Error message asserted");
-        }else{
+            logger.info("Error message asserted");
+        } else {
             Assert.assertTrue(false);
-            System.out.println("Error message not asserted");
+            logger.info("Error message not asserted");
         }
     }
+
     @Test(priority = 8)
     public void fillTheFormAndToVerifyCheckOutComplete() throws InterruptedException {
         webDriver.findElement(By.id("first-name")).sendKeys("Suraj");
@@ -134,11 +131,11 @@ public class TC_LoginTest_001 extends BaseClass {
         webDriver.findElement(By.id("postal-code")).sendKeys("33000");
         webDriver.findElement(By.id("continue")).click();
         webDriver.findElement(By.id("finish")).click();
-        String message=webDriver.findElement(By.xpath("//span[@class='title']")).getText();
-        if(message.equals("Checkout: Complete!")){
-            System.out.println("Check Out Complete");
-        }else{
-            System.out.println("CheckOut in complete");
+        String message = webDriver.findElement(By.xpath("//span[@class='title']")).getText();
+        if (message.equals("Checkout: Complete!")) {
+            logger.info("Check Out Complete");
+        } else {
+            logger.info("CheckOut in complete");
         }
         Thread.sleep(2000);
     }
@@ -146,14 +143,16 @@ public class TC_LoginTest_001 extends BaseClass {
     @Test(priority = 9)
     public void toLogoutAndVerifyLoginPage() throws InterruptedException {
         webDriver.findElement(By.id("react-burger-menu-btn")).click();
-        System.out.println("Logging Out of Site");
+        logger.info("Logging Out of Site");
+        Thread.sleep(2000);
         webDriver.findElement(By.id("logout_sidebar_link")).click();
-        System.out.println("Logged Out");
-        List<WebElement> verify=webDriver.findElements(By.xpath("//input[@id='login-button']"));
-        if(verify.size()==1){
-            System.out.println("We came to Login page");
-        }else{
-            System.out.println("Error after Logout");
+        logger.info("Logged Out");
+        List<WebElement> verify = webDriver.findElements(By.xpath("//input[@id='login-button']"));
+        logger.info(verify.size());
+        if (verify.size() == 1) {
+            logger.info("We came to Login page");
+        } else {
+            logger.info("Error after Logout");
         }
         Thread.sleep(2000);
     }
